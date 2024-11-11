79b351f76f099528f82bc8bdf5f13d6d12b35333
unknown <tyler.igwe@gmail.com>
2024-11-11 08:13:48 +0100

    cors

---
+++ config/corsList.js
@@ -0,0 +1,9 @@
+const corsOptions = { 
+    
+    origin: 'http://yoursite.com',
+    credentials: true,
+    optionSuccessStatus: 200 
+
+}
+
+module.exports = corsOptions
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const cors = require('cors')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const corsOptions = require('./config/corsList')
 
 
-const app = express()
+const app = express();
 app.use(express.json());
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-
-
+if (connectDB()) {
+  app.listen(process.env.PORT, () => {
+    console.log(`server Started and listening on ${process.env.PORT}`);
+  });
+}else{
+    console.log(`something went wrong with the DB`)
+}
