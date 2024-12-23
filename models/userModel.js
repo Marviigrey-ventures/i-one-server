925d7f74fb97dcf38b0b69ba76d745db0f5ef39b
unknown <tyler.igwe@gmail.com>
2024-12-23 12:06:35 +0100

    lates

--- models/userModel.js
+++ models/userModel.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose')
-
-const Schema = mongoose.Schema
-
-const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    isAdmin:{
-        type: Boolean,
-        required: true,
-        default: false
-    },
-   otp: {
-    type: String,
-    default: null
-   },
-   otpExpiration: {
-    type: Date,
-    default: null
-   },
-   otpVerified: {
-    type: Boolean,
-    default: null
-   }
-},)
-
-module.exports = mongoose.model('User', userSchema)
-=======
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
@@ -79,4 +44,3 @@ const userSchema = new Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
->>>>>>> upstream/master
