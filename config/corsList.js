07ae03805290ed4125c46a87de8fe797ea4dffb0
chidiebere1 <chidiebere@zap.africa>
2025-05-15 10:21:54 +0100

    allowing all origins

--- config/corsList.js
+++ config/corsList.js
@@ -1,9 +1,12 @@
-const corsOptions = { 
-    
-    origin: 'http://yoursite.com',
-    credentials: true,
-    optionSuccessStatus: 200 
-
-}
-
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (origin) {
+      callback(null, origin); // allow all origins that send an Origin header
+    } else {
+      callback(new Error('Origin not allowed'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+};
 module.exports = corsOptions
--- utils/createToken.js
+++ utils/createToken.js
@@ -7,12 +7,13 @@ const token = jwt.sign({userId}, process.env.JWT_SECRET, {
     expiresIn: '30d',
 })
   //set JWT as HTTP-only cookie
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV != "development",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-  })
+res.cookie("jwt", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV !== "development",
+  sameSite: "none",
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+});
+
 
   return token
 }
